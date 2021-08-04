23b4148982d358dac82e54e1dc49f68cd436e591
EC2 Default User <ec2-user@ip-172-31-19-172.us-east-2.compute.internal>
2021-08-04 08:13:35 +0000

    first commit

--- spec/lib/bowling_spec.rb
+++ spec/lib/bowling_spec.rb
@@ -1,28 +1,31 @@
 require "bowling"
 
 describe "ボリングのスコア計算" do
+  #インスタンスの生成を共通化
+  before do
+    @game = Bowling.new
+  end
   describe "全体の合計" do
     context "全ての投球がガターだった場合" do
       it "0になること" do
-        @game = Bowling.new
-
-        20.times do
-          @game.add_score(0)
-        end
-
+        add_many_scores(20,0)
         expect(@game.total_score).to eq 0
       end
     end
     context "全ての投球で1ピンずつ倒した場合" do
       it "20になること" do
-        @game = Bowling.new
-
-        20.times do
-          @game.add_score(1)
-        end
-
+        add_many_scores(20,1)
         expect(@game.total_score).to eq 20
       end
     end
   end
 end
+private
+# 複数回のスコア追加をまとめて実行する
+def add_many_scores(count, pins)
+    count.times do
+          @game.add_score(pins)
+    end
+end
+
+
